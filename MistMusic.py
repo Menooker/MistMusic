2e1cd9b9041ed1695dbc87fba3db96e32f788ee8
Menooker <myjisgreat@live.cn>
2017-04-18 15:38:09 +0800

    add donwloader

--- MistMusic.py
+++ MistMusic.py
@@ -8,7 +8,6 @@ CrawlerCore.put_song(380246)#小手拉大手
 CrawlerCore.put_song(130762)#后来
 CrawlerCore.put_song(378181)#爱我别走
 CrawlerCore.put_song(1769901638)#Rolling In The Deep
-CrawlerCore.put_song(75250)
 CrawlerCore.put_song(2067242)#好久不见
 #CrawlerCore.put_song(1769938907)#An Der Schönen Blauen Donau Op.314
 threads=[]
